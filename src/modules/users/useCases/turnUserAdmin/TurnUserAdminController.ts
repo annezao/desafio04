96d8d5e78bfef9b70508a05de0a05be324b37ae6
Arianne <arianne.macena@gmail.com>
2021-08-10 20:47:05 -0300

    implementando funcionalidades de tornar usuario admin e listar dados usuario

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,14 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = request.header('user_id');
+
+    try {
+      const users = this.turnUserAdminUseCase.execute({ user_id });
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
